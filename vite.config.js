cae738bbd79d2a94cb0d658764949a9652f365b3
Manaswini-118 <manaswinibatthula@gmail.com>
2025-11-03 09:44:45 +0530

    Add Vite configuration and .gitignore file

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+Vite.config.js 
+import { defineConfig } from 'vite' 
+import react from '@vitejs/plugin-react' 
+// https://vite.dev/config/ 
+export default defineConfig({ 
+plugins: [react()],}) 
+.gitignore 
+# Logs 
+logs 
+*.log 
+npm-debug.log* 
+yarn-debug.log* 
+yarn-error.log* 
+pnpm-debug.log* 
+lerna-debug.log* 
+node_modules 
+dist 
+dist-ssr 
+*.local 
+# Editor directories and files 
+.vscode/* 
+!.vscode/extensions.json 
+.idea 
+.DS_Store 
+*.suo 
+*.ntvs* 
+*.njsproj 
+*.sln 
+*.sw?
